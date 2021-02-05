ee7944f0515d36d9e9fa87b7f03044c9be9f795e
Victor Teka <ambukateka@gmail.com>
2021-02-05 23:29:46 +0300

    Setup Koin

---
+++ app/src/main/java/victorteka/github/io/koin_tutorial/App.kt
@@ -0,0 +1,19 @@
+package victorteka.github.io.koin_tutorial
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import victorteka.github.io.koin_tutorial.di.module.appModule
+import victorteka.github.io.koin_tutorial.di.module.repoModule
+import victorteka.github.io.koin_tutorial.di.module.viewModelModule
+
+class App: Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(listOf(appModule, viewModelModule, repoModule))
+        }
+    }
+}
